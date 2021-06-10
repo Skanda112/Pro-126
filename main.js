d31a10f9a786c27967f33cef1e4ce41ab28c3ce2
Skanda112 <72615563+Skanda112@users.noreply.github.com>
2021-06-10 19:23:26 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
-function setup()
-{
-    canvas = createCanvas(500, 400);
-    canvas.position(400, 100);
 
-    video = createCapture(VIDEO);
-    video.hide()
+song1 = "";
+song2 = "";
+
+
+function preload()
+{
+	song1 = loadSound("Avengers_one.mp3");
+	song2 = loadSound("music.mp3");
 }
+function setup() {
+	canvas =  createCanvas(300, 400);
+	canvas.center();
+
+	video = createCapture(VIDEO);
+	video.hide();
+
+	}
 
 function draw()
 {
-    image(video, 0, 0, 500, 400);
+    image(video, 0, 0, 300, 400);
 }
