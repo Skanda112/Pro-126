bc9716696eaddacd708693e703ccc7f485c40f60
Skanda112 <72615563+Skanda112@users.noreply.github.com>
2021-06-15 16:44:48 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,23 +1,52 @@
-
-song1 = "";
-song2 = "";
-
+song = "";
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 function preload()
 {
-	song1 = loadSound("Avengers_one.mp3");
-	song2 = loadSound("music.mp3");
+	song = loadSound("Avengers_one.mp3")
 }
 function setup() {
-	canvas =  createCanvas(300, 400);
+	canvas =  createCanvas(600, 500);
 	canvas.center();
 
 	video = createCapture(VIDEO);
 	video.hide();
 
-	}
+	poseNet = ml5.poseNet(video, modelLoaded);
+	poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded()
+{
+	console.log('PoseNet is initialized');
+}
 
 function draw()
 {
-    image(video, 0, 0, 300, 400);
+    image(video, 0, 0, 600, 500);
+}
+
+function play()
+{
+	song.play();
+	song.setVolume(1);
+	song.rate(1);
+}
+
+function gotPoses(results)
+{
+	if(results.length > 1)
+	{
+		console.log(results);
+		leftWristX = results[0].pose.leftWrist.x;
+		leftWristY = results[0].pose.leftWrist.y;
+		console.log("LeftWristX = " + leftWristX + "LeftWristY" + leftWristY);
+
+		rightWristX = results[0].pose.rightWrist.x;
+	    rightWristY = results[0].pose.rightWrist.y;
+		console.log("rightWristX = " + rightWristX + "rightWristY" + rightWristY);
+	}
 }
